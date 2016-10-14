ea7f28998dd96f13858b7ca417d781f1ebbf8e69
saikamalkola <saikamalkola@gmail.com>
2016-10-14 21:35:09 +0530

    Update pushbutton_python.ino

--- src/pushbutton_python.ino
+++ src/pushbutton_python.ino
@@ -3,10 +3,11 @@ void setup() {
 pinMode(8,INPUT);
 Serial.begin(9600);
 }
+int count=0;
 void loop() {
-  // put your main code here, to run repeatedl
+  // put your main code here, to run repeatedly:
   if(digitalRead(8)==HIGH)
   count++;
 Serial.println(count);
-
+delay(300);
 }
